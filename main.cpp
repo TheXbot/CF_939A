d9cf0c53313a7367caf16895c650dd6a3a9ffcf4
Nikita Struchkov <struchkov-2@mail.ru>
2018-03-02 13:37:45 +0300

    Correct answer

--- main.cpp
+++ main.cpp
@@ -10,10 +10,11 @@ int main() {
     }
     int temp;
     bool flag = false;
-    for(int i = 0; i < num; i++) {
+    for(int i = 0; i < num; i++) { //была ошибка с индексацией массива
         temp = mas[i];
-        temp = mas[temp];
-        if (i == mas[temp]) flag = true;
+        temp = mas[temp - 1];
+        temp = mas[temp - 1];
+        if (i == (temp - 1)) flag = true;
     }
     if (flag) cout << "YES";
         else cout << "NO";
